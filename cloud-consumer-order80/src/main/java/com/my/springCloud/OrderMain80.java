40939349c96b9664fbdd201e752d1a478dab03a8
criuszdl <1469625954@qq.com>
2022-02-18 16:38:27 +0800

    增加Eureka集群（高可用）

--- cloud-consumer-order80/src/main/java/com/my/springCloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/my/springCloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.my.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ZDL
  * @create 2022-02-17 15:00
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-eureka-server7002/src/main/java/com/my/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/my/springcloud/EurekaMain7002.java
@@ -2,16 +2,18 @@ package com.my.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ZDL
- * @create 2022-02-16 22:15
+ * @create 2022-02-18 16:24
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
+
 }
