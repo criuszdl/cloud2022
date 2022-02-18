5b9ec0bca3e83afabe85fc309da8c2e5f3960189
criuszdl <1469625954@qq.com>
2022-02-18 16:02:18 +0800

    增加Eureka注册中心模块（单机版）

---
+++ cloud-eureka-server7001/src/main/java/com/my/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ZDL
+ * @create 2022-02-18 15:50
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+
+}
