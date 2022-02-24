a4ba12d892cde657547e617518102fd5f8a16713
criuszdl <1469625954@qq.com>
2022-02-24 22:17:56 +0800

    增加Hystrix-服务监控dashboard demo

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/my/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author ZDL
+ * @create 2022-02-24 21:54
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+}
