35af1b68f4e0ef6d4095ab56f4c1a2c06a47d41b
criuszdl <1469625954@qq.com>
2022-03-07 15:43:18 +0800

    增加分布式配置config-动态刷新全局配置

---
+++ cloud-config-client-3366/src/main/java/com/my/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ZDL
+ * @create 2022-03-04 15:08
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
