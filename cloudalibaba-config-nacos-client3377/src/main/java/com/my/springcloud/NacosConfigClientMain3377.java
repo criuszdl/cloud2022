ab8bf6059bddf42e581c53e186f21dd42c3c4989
criuszdl <1469625954@qq.com>
2022-03-09 17:09:40 +0800

    增加Nacos作为配置中心-基础配置演示demo

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/my/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ZDL
+ * @create 2022-03-09 17:00
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
